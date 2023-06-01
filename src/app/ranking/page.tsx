99f605b7c84b8b13fc3b900ed3aa03898889e657
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-06-01 03:25:04 -0300

    componente RankingCard com fetch pronto

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,29 +1,59 @@
 'use client'
-import { useState } from "react"
+
+import { useState, useEffect } from "react"
+import RankingCard from "@/components/RankingCard";
 
 export default function Ranking() {
-  const [pokemon, setPokemon] = useState('charizard');
-
-  const pokemonFetch:Object = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((response)=>{response.json})
-
 
   return(
-    <main className="bg-gradient-to-b from-red-600 to-red-900 flex flex-col min-h-screen min-w-full">
+    <main className="bg-gradient-to-b from-red-600 to-red-900 flex flex-col items-center
+    justify-evenly min-h-screen min-w-full">
       <span className="text-[8rem] text-center text-white font-bold block">TOP RANKING</span>
       <section id="rankings" className="flex w-full justify-around">
-        <article className="bg-white h-[700px] w-[30rem] rounded-2xl overflow-hidden border-[3px] border-black">
-          <div className="w-full min-h-8 h-16 outline outline-black outline-2 cursor-pointer hover:opacity-80 flex">
-            <div className="h-full w-20 bg-black text-white flex items-center justify-center text-4xl font-bold">
-              1º
-            </div>
-            <div className="flex-1 bg-red-600 h-full"></div>
-            <div className="h-full w-20">
-              
-            </div>
-          </div>
+        <article className="bg-white h-max w-[30rem] rounded-2xl overflow-hidden border-[3px] border-black">
+          <RankingCard rank='1'/>
+          <RankingCard rank='2'/>
+          <RankingCard rank='3'/>
+          <RankingCard rank='4'/>
+          <RankingCard rank='5'/>
+          <RankingCard rank='6'/>
+          <RankingCard rank='7'/>
+          <RankingCard rank='8'/>
+          <RankingCard rank='9'/>
+          <RankingCard rank='10'/>
+        </article>
+        <article className="bg-white h-max w-[30rem] rounded-2xl overflow-hidden border-[3px] border-black">
+          <RankingCard rank='1'/>
+          <RankingCard rank='2'/>
+          <RankingCard rank='3'/>
+          <RankingCard rank='4'/>
+          <RankingCard rank='5'/>
+          <RankingCard rank='6'/>
+          <RankingCard rank='7'/>
+          <RankingCard rank='8'/>
+          <RankingCard rank='9'/>
+          <RankingCard rank='10'/>
+        </article>
+        <article className="bg-white h-max w-[30rem] rounded-2xl overflow-hidden border-[3px] border-black ">
+          <RankingCard rank='1'/>
+          <RankingCard rank='2'/>
+          <RankingCard rank='3'/>
+          <RankingCard rank='4'/>
+          <RankingCard rank='5'/>
+          <RankingCard rank='6'/>
+          <RankingCard rank='7'/>
+          <RankingCard rank='8'/>
+          <RankingCard rank='9'/>
+          <RankingCard rank='10'/>
         </article>
       </section>
+      <a 
+        href='/' 
+        className='h-14 w-max p-4 cursor-pointer flex items-center text-3xl font-bold rounded-md bg-white hover:scale-105 shadow-md'
+        target='_self'
+      >
+        Voltar {'<-'}
+      </a>
     </main>
   )
 }
---
+++ src/components/RankingCard.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+
+export default function RankingCard(props:any) {
+
+  const [pokemon, setPokemon] = useState<Object | any>({});
+  const [pokemonImage, setPokemonImage] = useState<string>('');
+  
+  const pokemonFetch = async (pokemon:string) => {
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await pokemonResponse.json();
+    setPokemon(data);
+  }
+
+  useEffect(()=>{
+    let randomPokemon = (Math.round(Math.random()*600))
+    pokemonFetch('' + randomPokemon);
+  }, []);
+
+  return (
+    <div className="w-full min-h-8 h-16 outline outline-black outline-2 cursor-pointer hover:opacity-80 flex">
+      <div className="h-full w-20 bg-black text-white flex items-center justify-center text-4xl font-bold">
+        {props.rank}
+      </div>
+      <div className="flex-1 flex items-center justify-center font-bold text-white text-[2.5rem] bg-red-600 h-full">
+        {pokemon.name ? pokemon.name.toUpperCase() : null}
+      </div>
+      <div className="h-full w-20 p-1 flex items-center justify-center">
+        {pokemon.name ? (
+          <img src={pokemon.sprites.front_default} alt="pokemon photo" />
+        ) : null}
+      </div>
+    </div>
+  );
+}
