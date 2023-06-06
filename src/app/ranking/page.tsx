9c940789799e7f5220de9c5dddd31f7a871cd8c9
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-06-05 22:53:56 -0300

    cleaned non-used imports

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState, useEffect } from "react"
 import RankingCard from "@/components/RankingCard";
 
 
--- src/components/PokemonModal.tsx
+++ src/components/PokemonModal.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 
 export default function PokemonModal (props:any) {
 
