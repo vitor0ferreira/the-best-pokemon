603298caa1688eabaf9bd926e57826f144654cf9
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-07-21 00:54:06 -0300

    More Responsive and Tailwind Adjusts

--- src/app/catalogue/components/PokemonCard.tsx
+++ src/app/catalogue/components/PokemonCard.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 export default function PokemonCard ({name}:any) {
 
   interface Pokemon {
-    id: Number,
-    name: String,
+    id: number,
+    name: string,
     sprites: {front_default: string}
   }
 
@@ -26,15 +26,15 @@ export default function PokemonCard ({name}:any) {
 
 
   return (
-    <div className="min-h-[80px] min-w-[80px] h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36 flex items-center justify-center m-2 cursor-pointer p-2 rounded-md shadow-md bg-gradient-to-b from-rose-100 to-teal-100 hover:scale-105">
-      {pokemonData.sprites.front_default ? <Image 
-        src={pokemonData.sprites?.front_default}
-        width={150}
-        height={150}
-        alt='pokemon photo'
-        className=''
-      /> : null}
-      
+    <div className="min-h-[80px] min-w-[80px] h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36 flex items-center justify-center m-1 cursor-pointer p-2 rounded-md shadow-md bg-gradient-to-b from-rose-100 to-teal-100 hover:scale-105">
+      <abbr title={pokemonData?.name.toUpperCase()}>
+        {pokemonData.sprites.front_default ? <Image 
+          src={pokemonData.sprites.front_default}
+          width={150}
+          height={150}
+          alt='pokemon photo'
+        /> : null}
+      </abbr>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="h-max w-full bg-white flex items-center justify-center flex-col px-2 py-2 sm:flex-row sm:gap-4 md:justify-evenly xl:justify-between xl:px-10">
-          <span className='italic font-extrabold text-blue-800 text-2xl whitespace-nowrap sm:text-3xl'><a href="">The Best Pokemon</a></span>
-          <nav className='flex font-bold text-lg gap-1 md:text-xl'>
+        <div className="h-max w-full bg-white flex items-center justify-center flex-col gap-1 px-2 py-2 sm:flex-row sm:gap-4 md:justify-evenly xl:justify-between xl:px-10">
+          <span className='italic font-extrabold text-blue-800 mt-2 text-2xl whitespace-nowrap sm:text-3xl sm:mt-0 md:text-4xl lg:text-5xl'><a href="">The Best Pokemon</a></span>
+          <nav className='flex font-bold text-lg gap-1 md:text-xl lg:text-2xl xl:text-3xl'>
             <a href="/" className='hover:bg-red-600 hover:text-white p-1 rounded-md'>Home</a>
             <a href="/catalogue" className='hover:bg-red-600 hover:text-white p-1 rounded-md'>Catalogue</a>
             <a href="/ranking" className='hover:bg-red-600 hover:text-white p-1 rounded-md'>Rankings</a>
