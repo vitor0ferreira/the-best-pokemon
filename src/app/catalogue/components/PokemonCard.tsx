e600b6265d4efd38e3715a0a0f29f1dad17bdadf
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-07-22 23:37:12 -0300

    testing timeout on alert

--- src/app/catalogue/components/PokemonCard.tsx
+++ src/app/catalogue/components/PokemonCard.tsx
@@ -32,12 +32,14 @@ export default function PokemonCard ({name}:any) {
         id = e.target.parentElement.parentElement.id
       }
     }
-    alert(id)
+    setTimeout(() => {
+      alert(id)
+    }, 500);
   }
 
 
   return (
-    <div id={pokemonData.id} className="min-h-[80px] min-w-[80px] h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36 flex items-center justify-center m-1 cursor-pointer p-2 rounded-md shadow-md bg-gradient-to-b from-rose-100 to-teal-100 hover:scale-105" onClick={handleClick}>
+    <div id={pokemonData.name} className="min-h-[80px] min-w-[80px] h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36 flex items-center justify-center m-1 cursor-pointer p-2 rounded-md shadow-md bg-gradient-to-b from-rose-100 to-teal-100 hover:scale-105" onClick={handleClick}>
       <abbr title={pokemonData?.name.toUpperCase()}>
         {pokemonData.sprites.front_default ? <Image 
           src={pokemonData.sprites.front_default}
