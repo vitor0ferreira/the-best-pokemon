03e236a789e4e7aec14d6494fefec9a347fa8589
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-07-24 00:22:29 -0300

    toFix: modal onclick (catalogue)

--- src/app/catalogue/components/modals/PokemonCardModal.tsx
+++ src/app/catalogue/components/modals/PokemonCardModal.tsx
@@ -1,10 +1,13 @@
 
-export default function PokemonCardModal ({props}:any) {
+export default function PokemonCardModal (props:any) {
+  const pokemon = props.pokemonData?.name
 
   return (
-    <div className="h-[100vh] w-[100vw]">
-      <div id="bg"></div>  
-      <div className="bg-white rounded-md h-[60vh] w-[60vw] absolute"></div>
-    </div>
+    <>
+      <div id="bg" className="absolute top-0 left-0 min-w-full w-full min-h-[full] h-full bg-black/30 z-auto"></div>  
+      <div className="bg-white rounded-md h-[60vh] w-[60vw] absolute bottom-0 top-0 z-999">
+        {props && pokemon}
+      </div>
+    </>
   )
 }
