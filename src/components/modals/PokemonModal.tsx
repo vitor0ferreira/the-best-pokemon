66f4ff4d18ae622e5b5a6f1388fe7e9822e8e4e1
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-12-18 00:42:18 -0300

    feat: modal adjusts

--- src/components/modals/PokemonModal.tsx
+++ src/components/modals/PokemonModal.tsx
@@ -3,7 +3,7 @@ export default function PokemonModal (props:any) {
 
   return (
       <>
-        <div className="bg-black/50 absolute top-0 left-0 w-full min-w-full h-full min-h-full z-auto" onClick={props.onclick}></div>
+        <div className="bg-black/50 fixed inset-0 z-auto" onClick={props.onclick}></div>
         <div className="bg-white absolute top-0 left-0 bottom-0 right-0 m-auto h-max w-max rounded-xl z-999 p-6 flex justify-start items-center gap-4">
           <span className="text-4xl font-bold">
             Confirm vote to: {props.pokemonData.name.toUpperCase()}
