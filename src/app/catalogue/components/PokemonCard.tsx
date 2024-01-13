c71d6507849322943c61966e194ac034c383b39a
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2024-01-13 17:25:44 -0300

    feat: add Infinite Scroll by button on catalogue page

--- src/app/catalogue/components/PokemonCard.tsx
+++ src/app/catalogue/components/PokemonCard.tsx
@@ -41,7 +41,7 @@ export default function PokemonCard ({name}:any) {
 
   return (
     <>
-      <div id={pokemonData.name} className="min-h-[80px] min-w-[80px] h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36 flex items-center justify-center m-1 cursor-pointer p-2 rounded-md shadow-md bg-gradient-to-b from-rose-100 to-teal-100 hover:scale-105" onClick={handleClick}>
+      <div id={pokemonData.name} className="min-h-[80px] min-w-[80px] h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36 flex items-center justify-center cursor-pointer p-2 rounded-md shadow-md bg-gradient-to-b from-rose-100 to-teal-100 hover:scale-105" onClick={handleClick}>
         <abbr title={pokemonData?.name.toUpperCase()}>
           {pokemonData.sprites.front_default ? <Image
             src={pokemonData.sprites.front_default}
--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -1,9 +1,23 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
-export default async function Catalogue () {
+export default function Catalogue () {
+  
+  const [pokemons, setPokemons] = useState<Array<Pokemon>>([])
+  const [offset, setOffset] = useState<number>(0)
+  
+  async function FetchPokemons () {
+    const pokemonsDynamicFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=100`);
+    const response = await pokemonsDynamicFetch.json();
+    setPokemons((previousPokemons) => [...previousPokemons, ...response.results])
+  }
 
-  const pokemonsFetch = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=100')
-  const pokemons = await pokemonsFetch.json();
+
+  useEffect(()=>{
+    FetchPokemons()
+  },[offset])
 
   interface Pokemon {
     name: string;
@@ -11,10 +25,18 @@ export default async function Catalogue () {
   }
 
   return (
-    <main className="bg-red-600 min-h-screen flex flex-wrap justify-center pt-4 pb-4">
-      {pokemons.results.map((pokemon:Pokemon)=>{
-        return <PokemonCard name={pokemon.name} key={pokemon.name} />
-      })}
+    <main className="bg-red-600 min-h-screen flex flex-col items-center p-4 gap-10">
+      <section className="flex flex-wrap w-full justify-center items-center gap-2">
+        {pokemons.map((pokemon:Pokemon)=>{
+          return <PokemonCard name={pokemon.name} key={pokemon.name} />
+        })}
+      </section>
+      <button className="bg-white flex p-2 text-lg font-bold m-2 rounded-md cursor-pointer hover:scale-105" 
+      onClick={() => {setOffset((prevOffset)=> prevOffset + 100)
+        if(offset >= 1299){
+          alert('All pokemons showed up on the display')
+        }
+      }}>Load more</button>
     </main>
   )
 }
