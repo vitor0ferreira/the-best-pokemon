b54d4f0c6f81db05b7ca3d3395dcd984936df3ea
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2024-01-26 02:05:51 -0300

    feat: Search Input and Filter on catalogue

--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -5,6 +5,7 @@ import PokemonCard from "./components/PokemonCard";
 
 export default function Catalogue () {
   
+  const [searchValue, setSearchValue] = useState('')
   const [pokemons, setPokemons] = useState<Array<Pokemon>>([])
   const [offset, setOffset] = useState<number>(0)
   
@@ -25,13 +26,29 @@ export default function Catalogue () {
   }
 
   return (
-    <main className="bg-red-600 min-h-screen flex flex-col items-center p-4 gap-10">
+    <main className="bg-red-600 min-h-screen flex flex-col items-center justify-start p-4 gap-10">
+
+      <input
+        type="text"
+        placeholder="Pesquise um pokemon..."
+        className="p-2 text-lg mt-4 rounded-sm focus:outline-none focus:placeholder:invisible"
+        value={searchValue}
+        onChange={(e)=>setSearchValue(e.target.value)}
+      />
+
       <section className="flex flex-wrap w-full justify-center items-center gap-2">
-        {pokemons.map((pokemon:Pokemon)=>{
+        {pokemons.filter((pokemon:Pokemon)=>{
+            const pokemonName = pokemon.name.toLowerCase();
+            const searchedPokemon = searchValue.toLowerCase();
+            return pokemonName.includes(searchedPokemon)
+          }).map((pokemon:Pokemon)=>{
+            return (<PokemonCard name={pokemon.name} key={pokemon.name} />)
+          })}
+        {/* {pokemons.map((pokemon:Pokemon)=>{
           return <PokemonCard name={pokemon.name} key={pokemon.name} />
-        })}
+        })} */}
       </section>
-      <button className="bg-white flex p-2 text-lg font-bold m-2 rounded-md cursor-pointer hover:scale-105" 
+      <button className="bg-white flex p-2 text-lg font-bold rounded-md cursor-pointer hover:scale-105 mb-6" 
       onClick={() => {setOffset((prevOffset)=> prevOffset + 100)
         if(offset >= 1299){
           alert('All pokemons showed up on the display')
