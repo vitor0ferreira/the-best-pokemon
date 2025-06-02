41518c35b608024cbe3897361f7c25095d0ddef2
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2025-06-01 23:04:28 -0300

    feat: add infinite scroll and fix the nextauth

--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -1,57 +1,91 @@
 'use client'
 
 import { ALL_POKEMON_LIST } from "@/constants/allPokemonList";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 interface Pokemon {
   name: string;
-  url: string
+  url: string;
 }
 
 export default function Catalogue () {
-  
-  const [searchValue, setSearchValue] = useState('')
-  const [pokemons, setPokemons] = useState<Array<Pokemon>>([])
-  const [offset, setOffset] = useState<number>(20)
 
-  const pokemonList = ALL_POKEMON_LIST.slice(0, offset);
+  const [searchValue, setSearchValue] = useState('');
+  const initialOffset = 27;
+  const [offset, setOffset] = useState<number>(initialOffset);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const observerTarget = useRef(null);
 
+
+  const filteredPokemonsList = ALL_POKEMON_LIST.filter((pokemon: Pokemon) => {
+    const pokemonName = pokemon.name.toLowerCase();
+    const searchedPokemon = searchValue.toLowerCase();
+    return pokemonName.includes(searchedPokemon);
+  });
+
+  const pokemonList = filteredPokemonsList.slice(0, offset);
+
+  useEffect(() => {
+    setOffset(initialOffset);
+  }, [searchValue]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting && !isLoading) {
+          if (offset < filteredPokemonsList.length) {
+            setIsLoading(true);
+            setOffset((prevOffset) => prevOffset + 20);
+            setTimeout(() => setIsLoading(false), 300);
+          } else if (offset >= filteredPokemonsList.length && filteredPokemonsList.length > 0) {
+            alert('All pokemons for this search have been displayed');
+          }
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    const currentTarget = observerTarget.current;
+    if (currentTarget) {
+      observer.observe(currentTarget);
+    }
+
+    return () => {
+      if (currentTarget) {
+        observer.unobserve(currentTarget);
+      }
+    };
+  }, [isLoading, offset, filteredPokemonsList.length]);
 
   return (
     <main className="bg-red-600 min-h-screen flex flex-col items-center justify-start p-4 gap-10">
-
       <input
         type="text"
         placeholder="Type a pokemon name..."
         className="p-2 text-lg mt-4 rounded-sm focus:outline-none focus:placeholder:invisible"
         value={searchValue}
-        onChange={(e)=>setSearchValue(e.target.value)}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
 
-      <section className="flex flex-wrap w-full justify-center items-center gap-2 z-10">
-        
-        {pokemonList.filter((pokemon:Pokemon)=>{
-
-            const pokemonName = pokemon.name.toLowerCase();
-            const searchedPokemon = searchValue.toLowerCase();
-            
-
-            return pokemonName.includes(searchedPokemon)
-          }).map((pokemon:Pokemon)=>{
-            const pokemonURLparts = pokemon.url.split('/')
-            const pokemonID = Number(pokemonURLparts[pokemonURLparts.length - 2])
-
-            return (<PokemonCard name={pokemon.name} key={pokemon.name} />)
-          })}
-
+      <section className="w-full flex flex-wrap items-center justify-center gap-2 z-10">
+        {pokemonList.map((pokemon: Pokemon) => {
+          const pokemonURLparts = pokemon.url.split('/')
+          const pokemonID = Number(pokemonURLparts[pokemonURLparts.length - 2])
+          return (<PokemonCard name={pokemon.name} id={pokemonID} key={pokemon.url} />)
+        })}
       </section>
-      <button className="bg-white flex p-2 text-lg font-bold rounded-md cursor-pointer hover:scale-105 mb-6" 
-      onClick={() => {setOffset((prevOffset)=> prevOffset + 20)
-        if(offset >= 1299){
-          alert('All pokemons showed up on the display')
-        }
-      }}>Load more</button>
+
+
+      {!isLoading && offset < filteredPokemonsList.length && (
+        <div ref={observerTarget} style={{ height: '50px', width: '100%' }}>
+          
+        </div>
+      )}
+
+      {offset >= filteredPokemonsList.length && filteredPokemonsList.length > 0 && pokemonList.length > 0 && (
+         <p className="text-white text-center my-4">Todos os Pokémon para esta busca foram exibidos.</p>
+      )}
     </main>
   )
 }
